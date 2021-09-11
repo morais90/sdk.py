af2da93cc4be3d10e087b9d0c3de7da3d6b277bc
Willian Morais <williandmorais@gmail.com>
2021-09-11 19:56:56 -0300

    feat: implement the Endpoint class

---
+++ sdk/endpoint.py
@@ -0,0 +1,31 @@
+from typing import Optional, Union
+
+from .data import APIOptions
+from .enums import HTTPMethod
+from .response import FormResponse, JSONResponse, Request
+
+
+class Endpoint:
+    _meta: APIOptions
+    collection_prefix: Optional[str]
+
+    def __init__(
+        self, name: Optional[str] = None, method: HTTPMethod = HTTPMethod.GET
+    ) -> None:
+        self.name = name
+        self.method = method
+
+    @property
+    def url(self) -> str:
+        base_url = self._meta.base_url.rstrip("/")
+        collection_prefix = self.collection_prefix or ""
+        endpoint = self.name.strip("/")
+
+        composed_url = [base_url, collection_prefix, endpoint]
+        return "/".join(composed_url)
+
+    def __call__(self, *args, **kwargs) -> Union[JSONResponse, FormResponse]:
+        request = Request(url=self.url)
+        method = getattr(request, self.method, None)
+
+        return method(*args, **kwargs)
