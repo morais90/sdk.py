dd202aa5e09e8a2e214bf55494ade18b3dc567eb
Willian Morais <williandmorais@gmail.com>
2021-09-11 19:51:21 -0300

    feat: implement the typing alias

---
+++ sdk/typing.py
@@ -0,0 +1,8 @@
+from typing import Dict, List, Union
+
+
+Header = Dict[str, str]
+QueryParams = Dict[str, Union[str, List[str]]]
+
+RawBody = Union[str, bytes]
+Body = Dict[str, Union[str, bool, int]]
