046555484e917ea147b669e3a3c06a7f3a624fd0
Willian Morais <williandmorais@gmail.com>
2021-09-12 17:15:09 -0300

    feat: set the APIOption as immutable

--- sdk/data.py
+++ sdk/data.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from .authentication import Authentication, APIKeyAuthentication
 
 
-@dataclass
+@dataclass(frozen=True)
 class APIOptions:
     base_url: str
     authentication_class: Authentication = APIKeyAuthentication
