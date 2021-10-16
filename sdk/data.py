ed381d680749d1d2c5e7b84a4decfc79f6e2663c
Willian Morais <williandmorais@gmail.com>
2021-10-16 09:52:24 -0300

    feat: add the kwargs as Meta options

--- sdk/data.py
+++ sdk/data.py
@@ -1,8 +1,11 @@
-from dataclasses import dataclass
-from .authentication import Authentication, APIKeyAuthentication
+from dataclasses import dataclass, field
+from typing import Any, Dict
+
+from .authentication import APIKeyAuthentication, Authentication
 
 
 @dataclass(frozen=True)
 class APIOptions:
     base_url: str
     authentication_class: Authentication = APIKeyAuthentication
+    kwargs: Dict[str, Any] = field(default_factory=dict)
