e220c1f01dcbd457a3246ee2e6db1556d41b8cb0
Willian Morais <williandmorais@gmail.com>
2021-11-07 11:33:51 -0300

    refactor: expose the classes to the sdk module

--- sdk/__init__.py
+++ sdk/__init__.py
@@ -1,4 +1,17 @@
 from .api import API
-from .authentication import APIKeyAuthentication
+from .authentication import APIKeyAuthentication, Authentication
 from .collection import Collection
 from .endpoint import Endpoint, HTTPMethod
+from .request import Request
+from .response import Response
+
+__all__ = [
+    "API",
+    "APIKeyAuthentication",
+    "Authentication",
+    "Collection",
+    "Endpoint",
+    "HTTPMethod",
+    "Request",
+    "Response",
+]
