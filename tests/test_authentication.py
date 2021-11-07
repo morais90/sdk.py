dff87eed1b52e5aa2fbd5e9d73cdd2322870d5d4
Willian Morais <williandmorais@gmail.com>
2021-11-07 11:34:33 -0300

    test: add the authentication tests

---
+++ tests/test_authentication.py
@@ -0,0 +1,22 @@
+from sdk import APIKeyAuthentication, Authentication, Request
+
+
+def test_authentication_should_return_an_unchanged_request():
+    request = Request(url="https://api.sdk.com")
+    authentication = Authentication()
+    authenticated_request = authentication.authenticate(request)
+
+    assert authenticated_request._headers == {}
+    assert authenticated_request._params == {}
+
+
+def test_api_authentication_should_return_the_authorization_header():
+    request = Request(url="https://api.sdk.com")
+    authentication = APIKeyAuthentication(
+        api_key="6d352e29-8d4c-4f61-b64e-b67b12c3808f"
+    )
+    authenticated_request = authentication.authenticate(request)
+
+    assert authenticated_request._headers == {
+        "Authorization": "Bearer 6d352e29-8d4c-4f61-b64e-b67b12c3808f"
+    }
