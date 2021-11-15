748e67e1acbff67cc9c5d35ec28fd9a297257cb4
Willian Morais <williandmorais@gmail.com>
2021-11-15 17:26:11 -0300

    test: add test cases for the Response

---
+++ tests/test_response.py
@@ -0,0 +1,23 @@
+from sdk import JSONResponse, Response
+
+
+def test_response_init():
+    response = Response(
+        data=b"just a response", status=200, headers={"cache-control": "no-cache"}
+    )
+
+    assert response.status == 200
+    assert response.headers == {"cache-control": "no-cache"}
+    assert response.raw_data == b"just a response"
+    assert response.data == b"just a response"
+
+
+def test_json_response_should_loads_the_content():
+    response = JSONResponse(
+        data=b'{"foo": "bar"}', status=200, headers={"cache-control": "no-cache"}
+    )
+
+    assert response.status == 200
+    assert response.headers == {"cache-control": "no-cache"}
+    assert response.raw_data == b'{"foo": "bar"}'
+    assert response.data == {"foo": "bar"}
